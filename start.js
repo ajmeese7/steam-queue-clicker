334a20e4eca4ab91f07ef76ac6852d61643cc4cb
Aaron Meese <ajmeese7@gmail.com>
2019-01-06 15:15:30 -0700

    Successfully clicks through one queue
    Still has problems starting the second queue, but hopefully that's just a small issue with the for loop or something. I don't want to have to manually call the clickThroughQueue() function more than once, but I will if I have to.

--- start.js
+++ start.js
@@ -17,7 +17,7 @@ function login(username, password) {
     .click('[class="btnv6_blue_hoverfade  btn_medium"]'); // NOTE: DO NOT remove the double space. It is required.
 
     // Change the middle value for the number of queues you want to go through
-    for (i = 0; i < 2; i++) {
+    for (i = 1; i < 3; i++) {
       clickThroughQueue();
     }
 }
@@ -25,67 +25,75 @@ function login(username, password) {
 var previousStatus; // "good" if the previous count was not equal to NaN
 function clickThroughQueue() {
   previousStatus = "good";
-  /*.wait(1000) // IMMEDATELY times out? '[class="begin_exploring"]'
-  .click('[class="begin_exploring"]') // For a normal queue
-  .click('[id="refresh_queue_btn"]') // For going to another queue after the first one*/
   browser
     .wait('[class="begin_exploring"]') // IMMEDATELY times out?
     .click('[class="begin_exploring"]') // For a normal queue
-    .click('[id="refresh_queue_btn"]') // For going to another queue after the first one
-    .wait(500) // '[class="btn_next_in_queue"]'
+    //.click('[id="refresh_queue_btn"]') // For going to another queue after the first one
+    .wait(500)
     .evaluate(function () {
-      var elem = document.getElementsByClassName('queue_sub_text')[0];
+      // Is this the search that I need to use? Should it be the next button itself?
+      var elem = document.getElementsByClassName('next_in_queue_content')[0];
       if (elem) {
-
-        // elem != null
-        return elem.innerText;
+        return true;
       } else {
         return null;
       }
     })
     .then(count => {
-      if (count != null) { count = parseInt(count.slice(1,3)); }
       if (!count) {
         previousStatus = "bad";
-        // TODO: Add exception if you check again and the value is back! STILL A PROBLEM!!! (i.e. double age verif. in a row)
       }
       goToNext();
     })
     .catch(error => {
-      console.error('An ERROR occured in clickThroughQueue():', error)
+      // The split is to remove all the trailing error lines after the first
+      console.error('An ERROR occured in clickThroughQueue():', error.toString().split(/\r?\n/)[0])
     });
 }
 
 function goToNext() {
-  //console.log("in goToNext()!");
   browser
-    .wait(2000) // 500?
-    .click('[class="btn_next_in_queue btn_next_in_queue_trigger"]') // In case there is an age limiter
-    .click('[class="next_in_queue_content"]') // Regular game page (without age verification)
-    .wait(2000) // .wait('[class="queue_sub_text"]')
+    .wait(2000)
     .evaluate(function () {
-      var elem = document.getElementsByClassName('queue_sub_text')[0];
-      if (elem) {
-        // https://stackoverflow.com/a/5515349
-        // elem != null
-        return elem.innerText;
+      var queueOver = document.getElementById('refresh_queue_btn');
+      if (!queueOver) {
+        return true;
       } else {
         return null;
       }
     })
-    .then(count => {
-      //console.log("in count!!!");
-      //console.log("count: " + count);
-      //if (count) { count = parseInt(count.slice(1,3)); }
-      if (previousStatus == "good" /*&& ( || count > 0)*/) {
-        //console.log("in second IF STATEMENT");
-          if (!count) {
-            previousStatus = "bad";
-          }
-          goToNext();
+    .then(continueQueue => {
+      if (continueQueue) {
+        return browser
+          .click('[class="btn_next_in_queue btn_next_in_queue_trigger"]') // In case there is an age limiter
+          .click('[class="next_in_queue_content"]') // Regular game page (without age verification)
+          .wait(2000)
+          .evaluate(function () {
+            var elem = document.getElementsByClassName('next_in_queue_content')[0];
+            if (elem) {
+              return true;
+            } else {
+              return null;
+            }
+          })
+          .then(count => {
+            if (previousStatus == "good") {
+                if (!count) {
+                  previousStatus = "bad";
+                }
+                goToNext();
+            }
+          })
+          .catch(error => {
+            console.error('An ERROR occured in goToNext() IF STATEMENT:', error.toString().split(/\r?\n/)[0])
+          })
+      } else {
+        console.log("queue is DONE, according to goToNext()...");
+        return browser
+          .click('[class="btnv6_lightblue_blue btn_medium"]');
       }
     })
     .catch(error => {
-      console.error('An ERROR occured in goToNext():', error)
+      console.error('An ERROR occured in goToNext():', error.toString().split(/\r?\n/)[0])
     })
 }
