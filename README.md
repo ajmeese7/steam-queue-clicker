10c69655948e3efc644fbfc30e480863be1f7121
Aaron Meese <ajmeese7@gmail.com>
2019-01-08 13:28:08 -0700

    Clicks through specified # of clues
    Now it just needs optimization and a better README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # steam-queue-clicker
-Automatically click through your Steam queue
+This script is used to automatically click through your Steam queues during
+sales to earn you trading cards.
+
+I know there are browser extensions and browser scripts that can be used to
+accomplish this same result, but I preferred not having to put anything in my
+browser. And to be fair, I also wanted to practice my automation skills with Nightmare.
+This script uses Electron to run an independent browser for you to click through
+the queues. 
--- start.js
+++ start.js
@@ -1,57 +1,51 @@
 var Nightmare = require('nightmare');
-var browser = new Nightmare({show: true, typeInterval: 75, pollInterval: 50}); // poll 250
+var browser = new Nightmare({show: true, typeInterval: 50, pollInterval: 50});
 // NOTE: How to make it use the installed Electron instead of re-downloading?
 
-var username = "YOUR_username_HERE";
+// IDEA: Add a way to cycle through multiple accounts for users with `botnets`
 var password = "YOUR_password_HERE";
 login(username, password);
-// NOTE: TEMPORARILY__ calling clickThroughQueue() from end of login to bypass async requirements
 
 function login(username, password) {
-  // NOTE: You must manually enter your Steam mobile guard code if applicable
   browser
     .goto('https://store.steampowered.com/login')
     .wait('[id="input_username"]')
     .type('[id="input_username"]', username)
     .type('[id="input_password"]', password)
-    .click('[class="btnv6_blue_hoverfade  btn_medium"]'); // NOTE: DO NOT remove the double space. It is required.
-
-    // Change the middle value for the number of queues you want to go through
-    for (i = 1; i < 3; i++) {
+    .click('[class="btnv6_blue_hoverfade  btn_medium"]') // NOTE: DO NOT remove the double space. It is required.
+    .wait('[id="home_maincap_v7"]') // This gives you 30 seconds to enter your Steam guard code (if applicable)
+    .then(function() {
       clickThroughQueue();
-    }
-}
-
-var previousStatus; // "good" if the previous count was not equal to NaN
-function clickThroughQueue() {
-  previousStatus = "good";
-  browser
-    .wait('[class="begin_exploring"]') // IMMEDATELY times out?
-    .click('[class="begin_exploring"]') // For a normal queue
-    //.click('[id="refresh_queue_btn"]') // For going to another queue after the first one
-    .wait(500)
-    .evaluate(function () {
-      // Is this the search that I need to use? Should it be the next button itself?
-      var elem = document.getElementsByClassName('next_in_queue_content')[0];
-      if (elem) {
-        return true;
-      } else {
-        return null;
-      }
-    })
-    .then(count => {
-      if (!count) {
-        previousStatus = "bad";
-      }
-      goToNext();
-    })
-    .catch(error => {
-      // The split is to remove all the trailing error lines after the first
-      console.error('An ERROR occured in clickThroughQueue():', error.toString().split(/\r?\n/)[0])
     });
 }
 
-function goToNext() {
+var queuesClickedThrough = 0;
+function clickThroughQueue() {
+  // NOTE: For some reason there is ~10 second delay between clicking queues
+  queuesClickedThrough++;
+
+  // NOTE: Change the 3 to however many queues you want to click through.
+  if (queuesClickedThrough <= 3) {
+    console.log("clickThroughQueue() time #: " + queuesClickedThrough);
+    if (queuesClickedThrough < 2) {
+      browser
+        .wait('[class="begin_exploring"]')
+        .click('[class="begin_exploring"]')
+        .then(function() {
+          continueQueue();
+        });
+    } else {
+      continueQueue();
+    }
+  } else {
+    // NOTE: Now this case isn't running, so Electron isn't closing. Test again w/ <3?
+    console.log("clickThroughQueue() is done!");
+    browser.wait(5000).end();
+  }
+}
+
+function continueQueue() {
+  // TODO: Optimize timings!!
   browser
     .wait(2000)
     .evaluate(function () {
@@ -62,38 +56,28 @@ function goToNext() {
         return null;
       }
     })
-    .then(continueQueue => {
-      if (continueQueue) {
+    .then(queueNotOver => {
+      if (queueNotOver) {
         return browser
           .click('[class="btn_next_in_queue btn_next_in_queue_trigger"]') // In case there is an age limiter
           .click('[class="next_in_queue_content"]') // Regular game page (without age verification)
-          .wait(2000)
-          .evaluate(function () {
-            var elem = document.getElementsByClassName('next_in_queue_content')[0];
-            if (elem) {
-              return true;
-            } else {
-              return null;
-            }
-          })
-          .then(count => {
-            if (previousStatus == "good") {
-                if (!count) {
-                  previousStatus = "bad";
-                }
-                goToNext();
-            }
+          .then(function() {
+              continueQueue();
           })
           .catch(error => {
-            console.error('An ERROR occured in goToNext() IF STATEMENT:', error.toString().split(/\r?\n/)[0])
+            // The split is to remove all the trailing error lines after the first
+            console.error('An ERROR occured in continueQueue() IF STATEMENT:', error.toString().split(/\r?\n/)[0])
           })
       } else {
-        console.log("queue is DONE, according to goToNext()...");
+        console.log("queue is DONE...");
         return browser
-          .click('[class="btnv6_lightblue_blue btn_medium"]');
+          .click('[class="btnv6_lightblue_blue btn_medium"]')
+          .then(function() {
+              clickThroughQueue();
+          });
       }
     })
     .catch(error => {
-      console.error('An ERROR occured in goToNext():', error.toString().split(/\r?\n/)[0])
+      console.error('An ERROR occured in continueQueue():', error.toString().split(/\r?\n/)[0])
     })
 }
